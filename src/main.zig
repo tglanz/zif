97c5078d1bb072227a12faa7def80d18790e09d4
Tal Glanzman <talglanzman@gmail.com>
2020-04-11 06:20:29 -0700

    windows, + sprites

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,43 @@
 const std = @import("std");
 const c = @import("c.zig");
 
+const debug = std.debug;
+
+const WindowConfig = struct {
+    width: u32,
+    height: u32,
+    title: []const u8,
+};
+
+const SpriteSheet = struct {
+    uri: []const u8,
+    rows: usize,
+    cols: usize,
+    sprite_width: usize,
+    sprite_height: usize,
+};
+
 pub fn main() void {
 
-    var vec2 = c.rl.Vector2 {
-        .x = 3,
-        .y = 4,
+    debug.warn("1\n", .{});
+
+    const window_config = WindowConfig {
+        .title = "zif",
+        .width = 800,
+        .height = 600,
     };
 
-    vec2.x = 4;
+    debug.warn("2\n", .{});
 
-    std.debug.warn("{}\n", .{vec2});
+    const digits: SpriteSheet = .{
+        .uri = "resources/spritesheets/digits.png",
+        .rows = 4,
+        .cols = 3,
+        .sprite_width = 32,
+        .sprite_height = 32,
+    };
+
+    debug.warn("3\n", .{});
+
+    c.rl.InitWindow(123, 123, "asd");
 }
